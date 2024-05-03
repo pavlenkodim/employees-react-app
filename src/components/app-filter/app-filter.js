649ad2e77964de0698dd399836a64486dfbe18a1
pavlenkodim <pavlenkodim@mail.ru>
2024-05-03 09:52:20 +0500

    Scss and styled my project

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,12 +2,12 @@ import './app-filter.css';
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: 'all', label: 'Все сотрудники'},
-        {name: 'rise', label: 'На повышение'},
-        {name: 'moreThen1000', label: 'Заработная плата больше 1000$'}
+        {name: 'all', label: 'Все сотрудники', colored: false },
+        {name: 'rise', label: 'На повышение', colored: false },
+        {name: 'moreThen1000', label: 'Заработная плата больше 1000$', colored: true }
     ];
 
-    const buttons = buttonsData.map(({name, label}) => {
+    const buttons = buttonsData.map(({name, label, colored}) => {
         const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light';
         
@@ -15,7 +15,8 @@ const AppFilter = (props) => {
             <button type="button"
                     className={`btn ${clazz}`}
                     key={name}
-                    onClick={() => props.onFilterSelect(name)}>
+                    onClick={() => props.onFilterSelect(name)}
+                    style={colored ? {color: "red"} : null} >
                     {label}
             </button>
         )
